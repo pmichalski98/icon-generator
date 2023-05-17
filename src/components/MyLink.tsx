a176cdb3af1ac269ec43035752da210d6c7e382f
piotr <piotrek.priv.michalski@wp.pl>
2023-05-17 22:19:03 +0200

    add active link style

--- src/components/MyLink.tsx
+++ src/components/MyLink.tsx
@@ -1,6 +1,8 @@
 import React, { type ReactNode } from "react";
 import Link from "next/link";
 import classNames from "classnames";
+import { useRouter } from "next/router";
+import button from "~/components/Button";
 
 interface LinkProps {
   href: string;
@@ -8,7 +10,15 @@ interface LinkProps {
   children: ReactNode;
 }
 const MyLink = ({ href, className, children }: LinkProps) => {
-  const classes = classNames("font-medium hover:text-gray-300", className);
+  const router = useRouter();
+  const activeStyles =
+    router.pathname === href ? " border-b-4 rounded  border-rose-400/80 " : "";
+  const classes = classNames(
+    "font-medium hover:text-gray-300 ",
+    className,
+    activeStyles
+  );
+
   return (
     <Link className={classes} href={href}>
       {children}
